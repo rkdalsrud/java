3d7c044374f6026523a817c9ead9e74232de7df0
ITWILL <ITWILL@DESKTOP-UHULD9O>
2023-03-29 15:56:14 +0900

    2DAY

---
+++ java_1/src/basic/DataTypeApp.java
@@ -0,0 +1,8 @@
+package basic;
+
+public class DataTypeApp {
+	public static void main(String[] args) {
+		System.out.println("<<정수형(IntegerType)>>");
+		System.out.println("정수형(10진수))'"
+	}
+}
---
+++ java_1/src/basic/VarirableApp.java
@@ -0,0 +1,7 @@
+package basic;
+
+public class VarirableApp {
+	public static void main(String[] args) {
+	
+	}
+}
