aa72ee6a1e77e378537251e9411b898629f04c5b
ITWILL <ITWILL@DESKTOP-UHULD9O>
2023-03-29 12:31:51 +0900

    1day

---
+++ java_1/src/basic/HelloWorld.java
@@ -0,0 +1,9 @@
+package basic;
+public class HelloWorld {
+	public static void main(String[] agrs) {
+		System.out.print("java");
+		System.out.println("_Progmming");
+		System.out.println("Goood Luck!!!");
+
+	}		
+}
