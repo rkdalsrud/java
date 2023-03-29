52fc2bac6011b1ac0b8429b79c19b8739919758b
ITWILL <ITWILL@DESKTOP-UHULD9O>
2023-03-29 16:10:03 +0900

    2DAY

--- java_1/src/basic/VarirableApp.java
+++ java_1/src/basic/VarirableApp.java
@@ -5,7 +5,9 @@ public class VarirableApp {
 		int  su;
 		su=100;
 	System.out.print("초기값=");
+	System.out.println(su);//변수에 저장된 값을 화면에 출력
+	
+	System.out.print("변경값=");
 	System.out.println(su);
-		
 	}
 }
