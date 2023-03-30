b69196e2539e6b1ee1b83761648690e0954516c2
ITWILL <ITWILL@DESKTOP-UHULD9O>
2023-03-30 11:06:31 +0900

    2DAY

--- java_1/src/basic/DataTypeApp.java
+++ java_1/src/basic/DataTypeApp.java
@@ -3,6 +3,37 @@ package basic;
 public class DataTypeApp {
 	public static void main(String[] args) {
 		System.out.println("<<정수형(IntegerType)>>");
-		System.out.println("정수형(10진수))'"
+		System.out.println("정수형(10진수 100) ="+100);//숫자값-10진수 정수형 리터럴
+		//println() 메소드는 정수값을 무조건 10진수로 변환하여 출력
+		System.out.println("정수형(8진수 100) ="+0100);//0숫자값-8진수 정수형 리터럴
+		System.out.println("정수형(16진수 100) ="+0x100);//0x100숫자값-16진수 정수형 리터럴
+		System.out.println("정수형(10진수 100) ="+100L);//숫자값 L - 10진수 정수형 리터럴(8Byte)-LongType
+		
+		System.out.println("정수값 ="+21221);
+		//4Byte로 표현 가능한 정수값 : -2147483615~2147483647
+		//=> Byte로 표현 불가능한 정수값을 사용한 경우 에러 발생 
+		//그럴 땐 숫자 뒤에 L붙여주면 에러 발생x
+		System.out.println("정수값 ="+2147483648L);
+		
+		byte a1=127;
+		short a2=32767;
+		int a3=2147483647;
+		System.out.println("a1="+a1);
+		//int a5=100L; 값에 대한 손실이 발생할 수 있으므로 에러 발생
+		System.out.println("==========================================");
+		System.out.println("실수값(4Byte) ="+12.3F); //실수값F >>4Byte 실수형 리터럴(FloatType)
+		System.out.println("실수값(8Byte) ="+12.3F); //실수값 >>8Byte 실수형 리터럴(DoubleType)
+		System.out.println("실수값(8Byte) ="+12.3F); //실수값 >>8Byte 실수형 리터럴(DoubleType)
+		System.out.println("실수값(8Byte) ="+0.000000123);
+		
+		//실수값을 지수형태로 표현하여 사용 가능
+		float b1=1.23456789F;//가수부를 표현하기 위한 크기(정밀도) : 7자리
+		//뒷자리 9가 오버플로어되서 9가 8에 영향을 미쳐 8이 9가 된다.
+		double b2=1.23456789;//가수부를 표현하기 위한 크기(정밀도):15자리
+
+		System.out.println("b1="+b1);
+		System.out.println("b2="+b2);
+		
+		
 	}
 }
--- java_1/src/basic/VarirableApp.java
+++ java_1/src/basic/VarirableApp.java
@@ -4,10 +4,39 @@ public class VarirableApp {
 	public static void main(String[] args) {
 		int  su;
 		su=100;
-	System.out.print("초기값=");
+	
+		System.out.print("초기값=");
 	System.out.println(su);//변수에 저장된 값을 화면에 출력
 	
+	su=200;
 	System.out.print("변경값=");
 	System.out.println(su);
+	
+	System.out.println("==========================================");
+	int num=100;//변수 선언 및 초기값 저장
+	
+	System.out.println("num="+num);
+	System.out.println("올해는 "+2+0+2+3+"년입니다.");
+	System.out.println(2+0+2+3+"년은 토끼띠해입니다.");
+	System.out.println(""+2+0+2+3+"년은 토끼띠해입니다.");
+	System.out.println("==========================================");
+
+	int num1=100, num2=200;
+	System.out.println("연산결과= "+num1+num2);
+	System.out.println("연산결과= "+(num1+num2));
+	System.out.println("연산결과= "+num1*num2);
+	System.out.println("연산결과="+(num1*num2)); //*할 경우 괄호 치는 것을 권장
+	System.out.println("==========================================");
+	int kor=88,eng=90;
+
+	int tot= kor+eng; //int tot=88+90 점수가 변동할 수 있기 때문에 앞처럼 써줘야 함, 연산 결과를 변수에 저장
+	System.out.println("점수합계="+tot);
+	System.out.println("==========================================");
+	//변수에 값이 저장되어 있지 않은 상태에서 변수를 사용할 경우 에러 발생
+	int count=10;
+	System.out.println("count= "+count);
+	
 	}
+
 }
+
