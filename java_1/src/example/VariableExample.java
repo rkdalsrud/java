94d8e3cb02530b2ba7432b438a0756ffbf394584
ITWILL <ITWILL@DESKTOP-UHULD9O>
2023-04-13 09:30:27 +0900

    수업

--- java_1/src/example/VariableExample.java
+++ java_1/src/example/VariableExample.java
@@ -4,11 +4,11 @@ public class VariableExample {
 	public static void main(String[] args) {
 		//가로의 길이가 7이고 세로의 길이가 10인 사각형의 넓이를 계산하여 출력하세요. nulbe
 		int garo=7, sero=10;
-		int nulbe=sero*garo;
+		int nulbe=garo*sero;
 		System.out.println("사각형의 넓이= "+nulbe);
 		System.out.println("======================================================");
 		
-		//높이가 9이고 밑변의 길이가 7인 삼각형의 넓이를 계산하여 출력하세요. area
+		//높이가 9이고 밑변의 길이가 7인 삼각형의 넓이를 계산하여 출력하세요. 
 		int 높이=9, 길이=7 ;
 		double 넓이= (double)높이*길이/2;
 		System.out.println("삼각형의 넓이="+넓이);
@@ -28,7 +28,7 @@ public class VariableExample {
 		
 		System.out.println("======================================================");
 		//이름이 [홍길동]인 학생이 국어점수 89점, 영어점수 93점, 수학점수 95점을 받은 경우
-		//총점과 평균을 계산하여 이름, 총점, 평균을 출력하세요.
+		//총점과 평균을 계산하여 이름, 총점, 평균을 출력하세요.name,kor,eng,mat,tot,avr
 		//단, 평균은 소숫점 한자리까지만 출력하고 나머지는 절삭 처리 하세요.
 		String name="홍길동";
 		int kor=89, eng=93, mat=95;
