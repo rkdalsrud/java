e658324e06013b4c638a559d53fc941486df4864
ITWILL <ITWILL@DESKTOP-UHULD9O>
2023-04-14 20:38:57 +0900

    14

--- java_example/src/java_example/Variable.java
+++ java_example/src/java_example/Variable.java
@@ -3,14 +3,8 @@ package java_example;
 public class Variable {
 	public static void main(String[] args) {
 	
-	int numofapples =123;
-	int sizeofbucket=10;
-	double numofbucket=(double)(numofapples/sizeofbucket*100+0.5)/100.;
-	
-	System.out.println(numofbucket);
-	System.out.println("===================================================================");
-	int num=10;
-	System.out.println((num=0?);
-		
+	int num=333;
+	int n=(num)
+	System.out.println();
 	}	
 }
